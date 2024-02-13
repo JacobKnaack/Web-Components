7768a2da242831fba34265d2bf7796dfad66cac7
JacobKnaack <jacobknck@gmail.com>
2024-02-13 10:33:35 -0800

    adds webpack config and barrel file for deployment

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Table from './components/table/Table.js';
+import Form from './components/form/Form.js';
+import Navigation from './components/navigation/Navigation.js';
+
+export {
+  Table,
+  Form,
+  Navigation
+}
